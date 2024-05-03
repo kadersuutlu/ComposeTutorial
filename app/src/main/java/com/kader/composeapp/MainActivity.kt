890ed138b199fe31ae540d5d1c22cf562f1057f0
kadersutlu <sutlukader@gmail.com>
2024-05-03 16:29:44 +0300

    Modifiers

--- app/src/main/java/com/kader/composeapp/MainActivity.kt
+++ app/src/main/java/com/kader/composeapp/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
@@ -20,28 +28,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            /*Column(
+            Column(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Green),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween
+                    .background(Color.Green)
+                    .fillMaxSize(0.5f)
+                    .fillMaxWidth()
+                    //.padding(16.dp)  //padding(top = 16.dp)
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+                //.width(300.dp),
+                //.requiredWidth(300.dp)
             ) {
-                Text("Hello")
+                Text(
+                    "Hello", modifier = Modifier.clickable {
+
+                    }
+                    //.offset(50.dp, 20.dp)
+                    /*.border(5.dp, Color.Yellow)
+                    .padding(5.dp)
+                    .offset(20.dp,20.dp)
+                    .border(10.dp, Color.Black)
+                    .padding(10.dp)*/
+                )
+                Spacer(modifier = Modifier.height(50.dp))
                 Text("World")
-                Text("Hello")
-            }*/
-            Row (
-                modifier = Modifier
-                    .width(300.dp)
-                    .fillMaxHeight(0.7f)
-                    .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically
-                ) {
-                Text("Hello")
-                Text("World")
-                Text("Hello")
             }
         }
     }
