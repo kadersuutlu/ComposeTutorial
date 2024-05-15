175eae8e7bcad46af402047a36e7c16f23bed8cf
kadersutlu <sutlukader@gmail.com>
2024-05-15 22:20:31 +0300

    update animations

--- app/src/main/java/com/kader/composeapp/MainActivity.kt
+++ app/src/main/java/com/kader/composeapp/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -38,46 +39,51 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var sizeState by remember { mutableStateOf(200.dp) }
-            val size by animateDpAsState(
-                targetValue = sizeState,
-                /*tween(
-                    durationMillis = 3000,
-                    delayMillis = 300,
-                    easing = LinearOutSlowInEasing
-                )*/
-                /*spring(
-                    Spring.DampingRatioHighBouncy
-                )*/
-                /*keyframes {
-                    durationMillis = 5000
-                    sizeState at 0 with LinearEasing
-                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
-                    sizeState * 2f at 5000
-                }*/
-                tween(
-                    durationMillis = 1000
-                )
-            )
-            val infiniteTransition = rememberInfiniteTransition()
-            val color by infiniteTransition.animateColor(
-                initialValue = Color.Red,
-                targetValue = Color.Green,
-                animationSpec = infiniteRepeatable(
-                    tween(durationMillis = 2000),
-                    repeatMode = RepeatMode.Reverse
-                )
-            )
-            Box(
-                modifier = Modifier
-                    .size(size)
-                    .background(color),
-                contentAlignment = Alignment.Center
-            ) {
-                Button(onClick = { sizeState += 50.dp }) {
-                    Text(text = "Increase Size")
-                }
-            }
+            animation()
+        }
+    }
+}
+
+@Composable
+private fun animation() {
+    var sizeState by remember { mutableStateOf(200.dp) }
+    val size by animateDpAsState(
+        targetValue = sizeState,
+        /*tween(
+                durationMillis = 3000,
+                delayMillis = 300,
+                easing = LinearOutSlowInEasing
+            )*/
+        /*spring(
+                Spring.DampingRatioHighBouncy
+            )*/
+        /*keyframes {
+                durationMillis = 5000
+                sizeState at 0 with LinearEasing
+                sizeState * 1.5f at 1000 with FastOutLinearInEasing
+                sizeState * 2f at 5000
+            }*/
+        tween(
+            durationMillis = 1000
+        )
+    )
+    val infiniteTransition = rememberInfiniteTransition()
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Red,
+        targetValue = Color.Green,
+        animationSpec = infiniteRepeatable(
+            tween(durationMillis = 2000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    Box(
+        modifier = Modifier
+            .size(size)
+            .background(color),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = { sizeState += 50.dp }) {
+            Text(text = "Increase Size")
         }
     }
 }
